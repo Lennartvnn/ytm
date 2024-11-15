0b23d528e07f73b5abc80c8352d481365f0bcf5d
Lennartvnn <lennart.von.nuis+github@gmail.com>
2024-11-15 01:31:50 +0000

    modified:   requirements.txt 	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
+from ytmusicapi import YTMusic
+
+ytm = YTMusic()
 
 st.title("🎈 My new app")
+st.write(ytm.search("hello", filter="songs"))
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
